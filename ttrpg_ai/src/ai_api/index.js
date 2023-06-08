b584552f083d24bd951f921fac0069d85da09742
AJ Read <aj.read13@gmail.com>
2023-06-08 19:22:42 -0400

    added basic api call, no connection, still needs user input

--- ttrpg_ai/src/ai_api/index.js
+++ ttrpg_ai/src/ai_api/index.js
@@ -1,7 +1,22 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+// require('dotenv').config()
+// import { Configuration, OpenAIApi } from "openai";
+// const configuration = new Configuration({
+//     organization: "",
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+const openaiApi = require('./openAiApi');
+
+const userInput = 'User input goes here'; // Replace with the actual user input
+
+(async () => {
+  try {
+    const response = await openaiApi(userInput);
+    console.log(response);
+    // Process and handle the API response as needed
+  } catch (error) {
+    // Handle errors
+  }
+})();
